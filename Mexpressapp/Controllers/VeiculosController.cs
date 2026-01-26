59e29b3df1470e952eed380ad82dd5449d92faa6
Mateus-denv <187669882+Mateus-denv@users.noreply.github.com>
2026-01-25 21:13:57 -0300

    Adicionar arquivos de projeto.

---
+++ Mexpressapp/Controllers/VeiculosController.cs
@@ -0,0 +1,42 @@
+﻿using Mexpressapp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mexpressapp.Controllers
+{
+    public class VeiculosController : Controller
+    {
+        public IActionResult Index()
+        {
+            var veiculos = new List<Veiculo>
+            {
+                new Veiculo
+                {
+                    Id = "1",
+                    Marca = "Toyota",
+                    Modelo = "Corolla",
+                    Cinlidrada = 1800,
+                    Cavalos = 140,
+                    Capacidadelitros = 13.2,
+                    Kmrodados = 50000,
+                    Disponivel = true,
+                    Preçodia = 150.00m,
+                    Placa = "ABC-1234"
+                },
+                new Veiculo
+                {
+                    Id = "2",
+                    Marca = "Honda",
+                    Modelo = "Civic",
+                    Cinlidrada = 2000,
+                    Cavalos = 158,
+                    Capacidadelitros = 12.4,
+                    Kmrodados = 30000,
+                    Disponivel = false,
+                    Preçodia = 170.00m,
+                    Placa = "DEF-5678"
+                }
+            };
+            return View(veiculos);
+        }
+    }
+}
---
+++ Mexpressapp/Models/Veiculo.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mexpressapp.Models
+{
+    public class Veiculo
+    {
+        public string Id { get; set; }
+        [Required]
+        public string Marca { get; set; }
+
+        [Required]
+        public string Modelo { get; set; }
+
+        [Required]
+        public int Cinlidrada { get; set; }
+
+        [Required]
+        public int Cavalos { get; set; }
+
+        [Required]
+        public double Capacidadelitros { get; set; }
+
+        [Required]
+        public int Ano { get; set; }
+
+        [Required]
+        public double Kmrodados { get; set; }
+
+        public bool Disponivel { get; set; } = true;
+
+        [Required]
+        [Display(Name = "Preço por dia")]
+        public decimal Preçodia { get; set; }
+        
+        [Required]
+        public string Placa { get; set; }
+    }
+}
