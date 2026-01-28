923073dfb96a59ec3dca84f34e39f152938ec587
Mateus-denv <187669882+Mateus-denv@users.noreply.github.com>
2026-01-28 09:24:35 -0300

    Foi feito a funcionalidade de cadastrar novos veiculos.

--- Mexpressapp/Controllers/VeiculosController.cs
+++ Mexpressapp/Controllers/VeiculosController.cs
@@ -17,5 +17,24 @@ namespace Mexpressapp.Controllers
             var veiculos = _context.Veiculos.Where(v => v.Disponivel).ToList();
             return View(veiculos);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] // Indica que este método responde a requisições POST
+        [ValidateAntiForgeryToken] // Protege contra CSRF
+        public IActionResult Create (Veiculo veiculo) // Recebe o objeto do formulário
+        {
+            if (ModelState.IsValid) // Valida
+            {
+                _context.Veiculos.Add(veiculo); // Adiciona ao contexto
+                _context.SaveChanges(); // Salva no banco
+                return RedirectToAction(nameof(Index)); // Redireciona para a lista
+            }
+
+            return View(veiculo); // Se inválido, retorna à view com o modelo
+        }
     }
 }
