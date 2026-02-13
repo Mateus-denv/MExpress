c1d4f862b0cb13673ecb2b10c0e9ba8f70d8d02b
Mateus-denv <187669882+Mateus-denv@users.noreply.github.com>
2026-02-13 10:32:59 -0300

    Feito novos campos no usuário

--- Mexpressapp/Models/ApplicationUser.cs
+++ Mexpressapp/Models/ApplicationUser.cs
@@ -1,10 +1,24 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace Mexpressapp.Models
 {
     public class ApplicationUser : IdentityUser
     {
-    
+        [Required]
+        public string NameCompleto { get; set; }
+        [Required]
+        public string CPF { get; set; }
+        [Required]
+        public DateTime? DataNascimento { get; set; }
+        [Required]
+        public string Endereco { get; set; }
+        public bool Ativo { get; set; } = true;   // Controle de acesso
+        [Required]
+        public DateTime DataCadastro { get; set; } = DateTime.Now; // Registro de data de cadastro
+
+        [Required]
+        public int CdsCNH { get; set; } // Codigo de validação da CNH (Carteira Nacional de Habilitação)
 
     }
 }
